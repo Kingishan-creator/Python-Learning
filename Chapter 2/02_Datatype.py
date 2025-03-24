8adb9cf5f23a459904795f149b5ad23e3fb78ee9
Kingishan-creator <juniorprogrammer7@gmail.com>
2025-03-24 12:58:44 +0530

    Intial Commit

---
+++ Chapter 2/02_Datatype.py
@@ -0,0 +1,5 @@
+a = 1 # This is a Interger type 
+b = 7.0 # This is Floating type 
+c = True # This is Boolean type 
+d = "Name" # This is String type 
+e = None # This iS None Type
---
+++ Chapter 2/03_Rules.py
@@ -0,0 +1,8 @@
+# Rule For Variables 
+# 1. A Variable can only contain numbers and alphabets and underscore 
+# 2. It can only start with alphabets and underscore 
+# 3. It Can't be started with a digits 
+# 4. There can't be a while space in it 
+_78a = 1 
+# 9a = 2  Error is coming 
+# 7 a = 2 
