9c5a080fe38a0f507af006575f04e4f4637efe93
Kingishan-creator <juniorprogrammer7@gmail.com>
2025-03-25 12:35:49 +0530

    Intial Commit

---
+++ Chapter 2/04_Operator.py
@@ -0,0 +1,21 @@
+# #  Operator 
+# 1. Arithmetic operators:- +, -, *, / etc.
+# 2. Assignment operators:- =, +=, -= etc.
+# 3. Comparison operators:- ==, >, >=, <, != etc.
+# 4. Logical operators:- and, or, not
+# Arithemtic 
+a = 3 
+b = 7 
+print(a+b)
+# Assignment Operators
+a = 4-2 
+b = 9-8 
+print(a-b)
+# Comparison Operators 
+a = 5 
+b = 7 
+print(a==b)
+# Logical operators 
+a = True 
+b = False 
+print (a and b)
---
+++ Chapter 2/05_Types.py
@@ -0,0 +1,10 @@
+a = 1 
+print(type(a)) # This is a interger 
+b = "Ishan"
+print(type(b)) # This is a string 
+c = 7.0 
+print(type(c)) # This is a floating 
+d = True
+print(type(d)) # This is a boolean 
+e = None
+print(type(e)) # This is None Type 
